30fe47fb507b38c4c9fc9de360b85d57f8df3c36
borba <rodrigoatjoin@gmail.com>
2022-01-20 20:28:32 -0300

    fix

--- src/main/java/com/borba/javaee/servlets/App.java
+++ src/main/java/com/borba/javaee/servlets/App.java
@@ -1,7 +1,5 @@
 package com.borba.javaee.servlets;
 
-import java.io.IOException;
-
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.ejb.Singleton;
@@ -13,15 +11,11 @@ import com.borba.javaee.jms.MyMessageProducer;
 @Startup
 public class App {
 	
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
 	@EJB
 	MyMessageProducer producer;
 	
 	@PostConstruct
-	public void start() throws IOException {
+	public void start() {
 		String message = "Hello message from JavaEE server using JMS";
 		producer.sendMessage(message);
 		System.out.println("Published the message " + message);
